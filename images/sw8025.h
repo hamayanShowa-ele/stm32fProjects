a92605df8e52aaf7637482326e9b0ab069d68967
hamayanShowa-ele <hamayan@Showa-ele.jp>
2020-05-24 23:12:08 +0900

    add image data

---
+++ images/sw8025.h
@@ -0,0 +1,52 @@
+/* ----------------------------------------
+  button images data
+  for STMicroelectronics SPL library
+
+  Copyright (c) 2020 hamayan (hamayan@showa-ele.jp).
+  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Created 2020 by hamayan (hamayan@showa-ele.jp)
+---------------------------------------- */
+
+#ifndef  __SW8025_H__
+#define  __SW8025_H__
+
+/* ----------------------------------------
+    defines
+---------------------------------------- */
+
+/* ----------------------------------------
+    prototypes 
+---------------------------------------- */
+
+/* ----------------------------------------
+    instances or global variables
+---------------------------------------- */
+extern unsigned long sw8025_Width;
+extern unsigned long sw8025_Height;
+extern const unsigned short sw8025_BMPTable[];
+
+extern unsigned long sw8025f_Width;
+extern unsigned long sw8025f_Height;
+extern const unsigned short sw8025f_BMPTable[];
+
+extern unsigned long sw8025p_Width;
+extern unsigned long sw8025p_Height;
+extern const unsigned short sw8025p_BMPTable[];
+
+#endif  /* __SW8025_H__ */
+
