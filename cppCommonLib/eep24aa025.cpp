7dbc65e746d5147661481a2f0633ba0fcb588c08
hamayanShowa-ele <hamayan@showa-ele.jp>
2020-10-15 09:25:49 +0900

    I2C シリアルEEPROM 24AA025のライブラリを追加。

---
+++ cppCommonLib/eep24aa025.cpp
@@ -0,0 +1,90 @@
+/* ----------------------------------------
+  EEPROM 24aa025 utilities
+  for STMicroelectronics SPL library
+
+  THE SOURCE CODE OF THE FOLLOWING url WAS MODIFIED FOR STM32F.
+  https://github.com/asukiaaa/AM2320_asukiaaa
+
+  Copyright (c) 2020 hamayan (hamayan@showa-ele.jp).
+  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Created 2020 by hamayan (hamayan@showa-ele.jp)
+---------------------------------------- */
+#include "eep24aa025.h"
+
+
+/* ----------------------------------------
+    constructor destructor
+---------------------------------------- */
+EEP24AA025::EEP24AA025()
+{
+}
+
+EEP24AA025::EEP24AA025( STM32F_I2C *_i2c, uint8_t adr )
+{
+  begin( _i2c, adr );
+}
+
+
+/* ----------------------------------------
+    begin and end
+---------------------------------------- */
+void EEP24AA025::begin( STM32F_I2C *_i2c, uint8_t adr )
+{
+  i2c = _i2c;
+  slave = adr;
+}
+
+/* ----------------------------------------
+   eeprom read
+---------------------------------------- */
+int EEP24AA025::read( uint8_t wordAdr, uint8_t *data, size_t sz )
+{
+  int ret;
+  /* While the bus is busy */
+  if( (ret = i2c->isBusy()) != I2C_SUCCESS )
+    return ret;
+
+  /* start condition */
+  if( (ret = i2c->startCondition()) != I2C_SUCCESS )
+    return ret;
+
+  /* Send module address and behave as receiver */
+  if( (ret = i2c->sendAddressAndBehaveAsReceiver( slave )) != I2C_SUCCESS )
+    return ret;
+
+  /* Send word address for 24aa025e */
+  if( (ret = i2c->sendWordAddress( wordAdr )) != I2C_SUCCESS )
+    return ret;
+
+  /* restart condition */
+  if( (ret = i2c->restartCondition()) != I2C_SUCCESS )
+    return ret;
+
+  /* Send control byte for 24aa025e */
+  if( (ret = i2c->sendAddressForRecieve( slave )) != I2C_SUCCESS )
+    return ret;
+
+  /* Get data from 24aa025e until data size */
+  if( (ret = i2c->getDataUntilSize( data, sz )) < 0 )
+    return ret;
+
+  /* Send STOP Condition */
+  i2c->stopCondition();
+
+  return ret;
+}
---
+++ cppCommonLib/eep24aa025.h
@@ -0,0 +1,57 @@
+/* ----------------------------------------
+  EEPROM 24aa025 utilities
+  for STMicroelectronics SPL library
+
+  THE SOURCE CODE OF THE FOLLOWING url WAS MODIFIED FOR STM32F.
+  https://github.com/asukiaaa/AM2320_asukiaaa
+
+  Copyright (c) 2020 hamayan (hamayan@showa-ele.jp).
+  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Created 2020 by hamayan (hamayan@showa-ele.jp)
+---------------------------------------- */
+
+#ifndef __24AA025_H__
+#define __24AA025_H__
+
+#include  <Wire.h>
+//#include  <softWire.h>
+
+extern "C"
+{
+  #include  <mul_tsk.h>
+  #include  <system.h>
+}
+
+#define  I2C_ADR_24AA025E48         0x50  /* 24AA025E48 mac address i2c eeprom */
+#define  MAC_ADDRESS_IN_24AA025E48  0xFA  /* 00:1E:C0 is Microchip Technology Inc. vender code. */
+
+class EEP24AA025 //: public STM32F_I2C
+{
+public:
+  EEP24AA025();
+  EEP24AA025( STM32F_I2C *_i2c, uint8_t adr );
+  void begin( STM32F_I2C *_i2c, uint8_t adr );
+
+  int  read( uint8_t wordAdr, uint8_t *data, size_t sz );
+
+private:
+  STM32F_I2C *i2c;
+  uint8_t slave;
+};
+
+#endif  /* __24AA025_H__ */
