79ba91552ff626d26ce5f0c3d018533f59c2a011
hamayanShowa-ele <hamayan@showa-ele.jp>
2021-04-27 13:44:04 +0900

    1703基板をGT-88のエミュレーション装置としたプロジェクトを追加。

---
+++ examples/1703_GT88_Emulate/src/derivertive.h
@@ -0,0 +1,241 @@
+/* ----------------------------------------
+  derivertive.h
+    designed by hamayan since 2009/05/21
+---------------------------------------- */
+#ifndef _DERIVERTIVE_H_
+#define _DERIVERTIVE_H_
+
+#include  <stm32fPeripheral.h>
+
+//#define  __BOARD_1303_1304_USE__
+//#define  __BOARD_0900_USE__
+//#define  __KANJI_USE__
+
+/*
+  defines for interrupt
+*/
+#define  BASE_PRIORITY      2
+#define  BASE_SUB_PRIORITY  0
+
+/*
+  defines for w5500.
+*/
+#define  _WIZCHIP_  5500
+#define  _WIZCHIP_IO_MODE_  _WIZCHIP_IO_MODE_SPI_VDM_
+#define  _WIZCHIP_CS_  PC7
+
+#define  __GPS_TYPE_FURUNO__
+//#define  __GPS_TYPE_MT3339__
+
+/*
+  time zone.
+*/
+#define TIMEZONE_JST    (9 * 60 * 60)
+
+/*
+ defines for usart and uart.
+*/
+#define  SCI_1_SND_BUFFER_SIZE  256
+#define  SCI_1_RCV_BUFFER_SIZE  256
+#define  SCI_2_SND_BUFFER_SIZE  0
+#define  SCI_2_RCV_BUFFER_SIZE  0
+#define  SCI_3_SND_BUFFER_SIZE  0
+#define  SCI_3_RCV_BUFFER_SIZE  0
+#define  SCI_4_SND_BUFFER_SIZE  0
+#define  SCI_4_RCV_BUFFER_SIZE  0
+#define  SCI_5_SND_BUFFER_SIZE  256
+#define  SCI_5_RCV_BUFFER_SIZE  256
+
+/*
+ defines for gpio pins.
+*/
+/* USART1 TXD RXD */
+#define  USART1_TXD_PORT      GPIOA
+#define  USART1_RXD_PORT      GPIOA
+#define  USART1_TXD_PIN       GPIO_Pin_9
+#define  USART1_RXD_PIN       GPIO_Pin_10
+#define  USART1_TXD           PA9
+#define  USART1_RXD           PA10
+
+/* USART2 TXD RXD */
+#define  USART2_TXD_PORT      GPIOA
+#define  USART2_RXD_PORT      GPIOA
+#define  USART2_TXD_PIN       GPIO_Pin_2
+#define  USART2_RXD_PIN       GPIO_Pin_3
+#define  USART2_TXD           PA2
+#define  USART2_RXD           PA3
+
+/* USART3 TXD RXD */
+#define  USART3_TXD_PORT      GPIOB
+#define  USART3_RXD_PORT      GPIOB
+#define  USART3_TXD_PIN       GPIO_Pin_10
+#define  USART3_RXD_PIN       GPIO_Pin_11
+#define  USART3_TXD           PB10
+#define  USART3_RXD           PB11
+
+/* UART4 TXD RXD */
+#define  UART4_TXD_PORT      GPIOC
+#define  UART4_RXD_PORT      GPIOC
+#define  UART4_TXD_PIN       GPIO_Pin_10
+#define  UART4_RXD_PIN       GPIO_Pin_11
+#define  UART4_TXD           PC10
+#define  UART4_RXD           PC11
+
+/* UART5 TXD RXD */
+#define  UART5_TXD_PORT      GPIOC
+#define  UART5_RXD_PORT      GPIOD
+#define  UART5_TXD_PIN       GPIO_Pin_12
+#define  UART5_RXD_PIN       GPIO_Pin_2
+#define  UART5_TXD           PC12
+#define  UART5_RXD           PD2
+
+/* SD CARD */
+#define  SD_CD      PC6  /* input */
+#define  SD_WP           /* input */
+#define  SD_SS      PB12  /* output */
+
+/* I2C1,I2C2 */
+#define  SCL2       PB10
+#define  SDA2       PB11
+#define  BOARD_1703_IC2_ADR  0x20  /* PCA8574PW */
+#define  BOARD_1703_IC3_ADR  0x21  /* PCA8574PW */
+#define  BOARD_1703_IC4_ADR  0x22  /* PCA8574PW */
+#define  BOARD_1703_U1_ADR   0x76  /* BME280 */
+//#define  BOARD_1703_IC7_ADR  0x50  /* 24AA025E48 mac address i2c eeprom */
+
+/* ETHERNET */
+#define  ETH_SPI    SPI2
+#define  ETH_RCC    RCC_APB1Periph_SPI2
+#define  ETH_GPIO   GPIOB
+#define  ETH_SCK_PIN  GPIO_Pin_13
+#define  ETH_MOSI_PIN GPIO_Pin_15
+#define  ETH_MISO_PIN GPIO_Pin_14
+#define  ETH_CS_PORT  GPIOC
+#define  ETH_CS_PIN   GPIO_Pin_7
+
+#define  W5500_INT  PC5   /* input */
+#define  W5500_CS   PC7   /* output */
+#define  W5500_RESET PC8   /* output */
+#define  SPI2_SCK_PORT   GPIOB
+#define  SPI2_SCK_PIN    GPIO_Pin_13
+#define  SPI2_MISO_PORT  GPIOB
+#define  SPI2_MISO_PIN   GPIO_Pin_14
+#define  SPI2_MOSI_PORT  GPIOB
+#define  SPI2_MOSI_PIN   GPIO_Pin_15
+#define  SPI2_SCK        PB13
+#define  SPI2_MISO       PB14
+#define  SPI2_MOSI       PB15
+#define  W5500_CS_PORT   GPIOC
+#define  W5500_CS_PIN    GPIO_Pin_7
+#define  W5500_CS_IS_0   W5500_CS_PORT->BSRR = (W5500_CS_PIN << 16)
+#define  W5500_CS_IS_1   W5500_CS_PORT->BSRR = (W5500_CS_PIN)
+
+/* SNTP */
+#define  SNTP_CLIENT_SOCKET  1
+#define  TIME_ZONE           40  /* Korea, East Timor, Russia (Irkutsk Oblast), Japan */
+
+/* ANOTHER */
+#define  PPS        PC9   /* input/output */
+#define  GCLK_RESET PA8   /* output */
+#define  DIVCLK     PA11   /* input/output */
+#define  GCLK       PA12   /* input/output */
+#define  SEG_A      PB3   /* output */
+#define  SEG_B      PB4   /* output */
+#define  SEG_C      PB5   /* output */
+#define  SEG_D      PB6   /* output */
+#define  SEG_E      PB7   /* output */
+#define  SEG_F      PB8   /* output */
+#define  SEG_G      PB9   /* output */
+#define  ACTLED     PC13   /* output */
+
+
+/*
+ defines for another types.
+*/
+#if 0
+typedef enum
+{
+  STDNULL=0,
+  STDOUT=1,
+  STDOUT2,
+  STDOUT3,
+  STDOUT4,
+  STDOUT5,
+  STDEND,
+} HANDLES;
+#define  STDIN                    STDOUT
+
+#define  Cpu_Delay100US(delay)    Dly100us(delay)
+#define  HEAPSIZE                 (10000)  /* look at ld file. */
+
+typedef struct
+{
+  const char *fn;
+  const char *dateTime;
+  unsigned long dataOffset;
+  void *data;
+  const char *auth;
+  void *exec;
+  unsigned long dataSize;
+} FILE_PROPERTIES;
+#endif
+
+
+/*
+ global variables.
+*/
+
+/*
+ prototypes.
+*/
+#if 0
+//#include  "mul_tsk.h"
+int xprintf(int fd,const char *fmt, ...);
+int _xwrite(int fd, char *str, int len);
+int _xread(int fd, char *str, int len);
+void gpioInit( void );
+void Dly10us( int delay );
+void Dly50us( int delay );
+void Dly100us( int delay );
+void cris_en( void );  // stop he interrupt.
+void cris_ex( void );  // start he interrupt.
+#endif
+
+/*
+ configures for multi tasks.
+*/
+#define  MAX_TASK_NUMBER  3
+enum GR_SAKURA_TSK_ID
+{
+  ID_stackMonitor,
+  ID_display,
+  ID_gt88,
+};
+
+#define  MAX_MBX_NUMBER    1
+enum GR_SAKURA_MBX_ID
+{
+  MBXID_dummy,
+};
+
+#define  MAX_SEM_NUMBER    1
+enum GR_SAKURA_SEM_ID
+{
+  SEMID_SPI2 = 1,
+};
+
+/*
+ debug messages.
+*/
+#define _DBG_passedFileAndLine() {strncpy(_file, __FILE__, sizeof(_file)); _line = __LINE__;}
+extern char _file[];
+extern int _line;
+
+
+#endif /*_DERIVERTIVE_H_*/
+
+
+/*********************************************************************************/
+/* end of file                                                                   */
+/*                                         designed by hamayan since 2009/05/21  */
+/*********************************************************************************/
---
+++ examples/1703_GT88_Emulate/src/main.cpp
@@ -0,0 +1,440 @@
+/* ----------------------------------------
+  stm32f103ret 1703 chronos examples code
+  for STMicroelectronics SPL library
+
+  Copyright (c) 2020 hamayan (hamayan@showa-ele.jp).
+  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Created 2020 by hamayan (hamayan@showa-ele.jp)
+
+
+  NOTICE! One second to the minus 26th power of 2 is 14.9 ns.
+---------------------------------------- */
+#include  <stm32f10x.h>
+#include  <time.h>
+#include  <math.h>
+#include  <Timer.h>
+#include  <USART_UART.h>
+#include  <I2C.h>
+#include  <SPI.h>
+#include  <led.h>
+#include  <strutil.h>
+#include  <pca8574.h>
+#include  <rom24aa025.h>
+#include  <boardUtility.h>
+#include  <wiznet.h>
+#include  <gpio.h>
+#include  <1703.h>
+#include  <1702.h>
+
+#include  <ntp.h>
+#include  <gnss.h>
+
+extern "C"
+{
+//  #include  <semihosting.h>
+  #include  <stdio.h>
+  #include  <string.h>
+  #include  <system.h>
+  #include  <mul_tsk.h>
+  #include  <stm32f_rtc.h>
+  #include  <wizchip_conf.h>
+  #include  <peripheral.h>
+//#include  <sntp.h>
+}
+
+
+/* ----------------------------------------
+    defines
+---------------------------------------- */
+
+/* ----------------------------------------
+    prototypes
+---------------------------------------- */
+void RCC_Configuration( void );
+void NVIC_Configuration( void );
+
+void msecInterrupt( void );
+void secInterrupt( void );
+
+/* ----------------------------------------
+    instances or global variables
+---------------------------------------- */
+SEM_OBJECT sem_obj[ MAX_SEM_NUMBER ];
+
+volatile SYSTIM systim;
+volatile time_t unixTime;
+volatile uint32_t pps_millisecond_counter;
+
+GPIO gpio;
+USART_UART usart1;
+USART_UART gpsSerial;
+SPI spi2;
+STM32F_I2C i2c2;
+BOARD  chronos;
+LED actLed;
+PCA8574 ic2;
+PCA8574 ic3;
+PCA8574 ic4;
+WIZNET wizchip;
+STM32F_TIMER tim1ms;
+STM32F_TIMER tim1s;
+BD1702 bd1702;
+
+uint16_t reload;
+#if 0
+uint8_t MAC[6];      // self hardware address
+//uint8_t IPVAL[4] = {192,168,100,230};  // self ip address
+uint8_t IPVAL[4] = {192,168,100,231};  // self ip address
+uint8_t GATEWAY[4] = {192,168,100,1};  // default gateway
+uint8_t SUBNET[4] = {255,255,255,0};   // sub net mask
+uint8_t DNS[4] = {192,168,100,1};      // primary dns server
+#else
+uint8_t xMAC[6];
+#endif
+
+/* ----------------------------------------
+    tasks
+---------------------------------------- */
+void ppsGenerator( void );  //ID_ppsGenerator
+void stackMonitor( void );
+void display( void );
+void gt88( void );
+
+uint8_t tsk1_stk[256 * 6];  // stack for task1
+uint8_t tsk2_stk[256 * 6];  // stack for task2
+uint8_t tsk3_stk[256 * 6];  // stack for task3
+
+/* ----------------------------------------
+    task initialize
+---------------------------------------- */
+void tsk_ini( void )
+{
+  reg_tsk( ID_stackMonitor,(void *)stackMonitor, tsk1_stk, sizeof(tsk1_stk), 0,0,0,0 );
+  reg_tsk( ID_display,(void *)display, tsk2_stk, sizeof(tsk2_stk), 0,0,0,0 );
+  reg_tsk( ID_gt88,(void *)gt88, tsk3_stk, sizeof(tsk3_stk), 0,0,0,0 );
+
+  sta_tsk( ID_stackMonitor );
+  sta_tsk( ID_display );
+  sta_tsk( ID_gt88 );
+}
+
+/* ----------------------------------------
+    main
+---------------------------------------- */
+int main(void)
+{
+  RCC_Configuration();
+  NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
+  NVIC_Configuration();
+
+  uint16_t rev = getRevID();
+  uint16_t dev = getDevID();
+  uint32_t uid[3];
+  getUniqueID( uid );
+
+  /* Setup SysTick Timer for 1 millisec interrupts, also enables Systick and Systick-Interrupt */
+  if( SysTick_Config(SystemCoreClock / 1000UL) )
+  {
+    /* Capture error */
+    while (1);
+  }
+
+  /* initialize GPIO and external CPU bus. */
+  chronos.gpioInit();
+  actLed.begin( ACTLED );
+
+//  if( RTC_Init() != 0 )
+  struct tm localTime;
+  if( true )
+  {
+//    struct tm localTime;
+    compileDate( &localTime.tm_year, &localTime.tm_mon, &localTime.tm_mday );
+    compileTime( &localTime.tm_hour, &localTime.tm_min, &localTime.tm_sec );
+    localTime.tm_year -= 1900;
+    localTime.tm_mon -= 1;
+
+    unixTime = mktime( &localTime ) - TIMEZONE_JST;
+    unixTime_Set( unixTime );
+  }
+
+  /* initialize serial */
+  usart1.begin( USART1, 115200UL, USART1_TXD, USART1_RXD );
+  usart1.print( "    1703 CHRONOS GT88 Emulate.\r\n" );
+  usart1.print( "    designed by hamayan.\r\n" );
+  usart1.printf( "    DEVICE ID:0x%04X REVISION:0x%04X UNIQUE ID:%02X:02X:02X.\r\n",
+    dev, rev, uid[0], uid[1], uid[2] );
+//  usart1.echo();  /* echo back is on. */
+
+  /* initialize i2c2 */
+  i2c2.begin( I2C2, SDA2, SCL2 );
+  /* initialize pca8574pw */
+  ic2.begin( &i2c2, BOARD_1703_IC2_ADR );
+  ic3.begin( &i2c2, BOARD_1703_IC3_ADR );
+  ic4.begin( &i2c2, BOARD_1703_IC4_ADR );
+  bd1702.begin( &ic2, &ic3, &ic4 );
+  bd1702.gpioInit();
+  bd1702.setHour( localTime.tm_hour );
+  bd1702.setMinute( localTime.tm_min );
+  bd1702.setSecond( localTime.tm_sec );
+  /* initialize 24AA025 */
+  EEP24AA025 eep( &i2c2, I2C_ADR_24AA025E48 );
+  int cnt = eep.read( MAC_ADDRESS_IN_24AA025E48, xMAC, sizeof(xMAC) );  /* 00:1E:C0 is Microchip Technology Inc. vender code. */
+  if( cnt != 6 )
+  {
+    usart1.printf( "  Mac address read from eeprom, but failed.\r\n" );
+    xMAC[0] = 0xDE; xMAC[1] = 0xAD; xMAC[2] = 0xBE; xMAC[3] = 0xEF; xMAC[4] = 0x00; xMAC[5] = 100;
+  }
+
+  /* initialize w5500. */
+  chronos.etherGpioInit();
+
+  /* initialize SPI2 */
+  spi2.begin( SPI2, SEMID_SPI2, SPI2_SCK, SPI2_MOSI, SPI2_MISO );  /* SPI?,SEMAPHORE,SCK,MOSI,MISO */
+
+  /* initialize wiznet w5500 */
+  wizchip.clearNetworkInfo();
+  wizchip.setMac( (const uint8_t *)xMAC );
+  wizchip.setIp( 192,168,100,231 );  // already 192,168,100,230 is used.
+  wizchip.setGateway( 192,168,100,1 );
+  wizchip.setSubnet( 255,255,255,0 );
+  wizchip.setDns( 192,168,100,1 );
+  /* The order of the memsize array is sending and receiving. */
+  static const uint8_t memsize[2][8] = {{2,2,2,2,2,2,2,2},{2,2,2,2,2,2,2,2}};  /* txsize,rxsize */
+  wizchip.begin( &spi2, (const uint8_t *)memsize, wizchip_select, wizchip_deselect );
+
+  /* initialize tasks and start dispatch. */
+  tsk_ini();  //
+  sta_rdq( ID_stackMonitor );  // start round robin
+
+  while(1) {}
+}
+
+
+/* ----------------------------------------
+    task stack monitor.
+---------------------------------------- */
+static unsigned int RemainStack( void *stk, unsigned int sz );
+
+void stackMonitor( void )
+{
+  SYSTIM baseTim = systim;
+//  time_t unxTimeBase = unixTime;
+  while( 1 )
+  {
+    /*stack report.*/
+    if( (systim - baseTim) >= 10 * 1000UL )
+    {
+      baseTim = systim;
+      usart1.printf( "  TASK1:%d/%d\r\n", RemainStack( tsk1_stk, sizeof(tsk1_stk) ), sizeof(tsk1_stk) );
+      usart1.printf( "  TASK2:%d/%d\r\n", RemainStack( tsk2_stk, sizeof(tsk2_stk) ), sizeof(tsk2_stk) );
+      usart1.printf( "  TASK3:%d/%d\r\n", RemainStack( tsk3_stk, sizeof(tsk3_stk) ), sizeof(tsk3_stk) );
+      char buffer[32];
+      usart1.printf( "    %s\r\n", localDateTimeString( buffer, unixTime ) );
+    }
+    if( (unixTime % 10) < 5 ) actLed.On();
+    else actLed.Off();
+
+    rot_rdq();
+  }
+}
+
+static unsigned int RemainStack( void *stk, unsigned int sz )
+{
+  unsigned int i;
+  char *ptr = (char *)stk;
+
+  for( i = 0; i < sz; i++ )
+  {
+    if( *ptr++ != 0 ) break;
+  }
+//  if( i == sz ) i = 0;
+
+  return sz - i;
+}
+
+
+/* ----------------------------------------
+    task 1PPS generator.
+---------------------------------------- */
+void ppsGenerator( void )
+{
+  tim1ms.begin( TIM1 );
+  tim1s.begin( TIM8 );
+  tim1ms.frequency( 1000 );
+  reload = tim1ms.getAutoReload();
+  tim1ms.master();
+  tim1s.slave( SRC_TIM1_DST_TIM8, 1, 1000 );
+  tim1s.pwm1( TIMx_CH4, PPS, 500 );
+
+  tim1ms.callBack( TIM1_INT_UP, msecInterrupt );
+  tim1s.callBack( TIM8_INT_UP, secInterrupt );
+  tim1ms.startInterrupt();
+  tim1s.startInterrupt();
+
+  tim1ms.start();
+  tim1s.start();
+
+  while( 0 )
+  {
+    rot_rdq();
+  }
+}
+
+/* ----------------------------------------
+    fixed size reload value.
+---------------------------------------- */
+
+/* ----------------------------------------
+    task display.
+---------------------------------------- */
+void display( void )
+{
+  while( 1 )
+  {
+    bd1702.segmentUpdate();
+    dly_tsk( 3UL );
+  }
+}
+
+/* ----------------------------------------
+    task gt88.
+    ntp server list
+      ntp.nict.jp
+      133.243.238.243
+      61.205.120.130
+      133.243.238.163
+      133.243.238.244
+      133.243.238.164
+      time.windows.com
+---------------------------------------- */
+void gt88( void )
+{
+  /* initialize gps serial. */
+#if  defined(  __GPS_TYPE_FURUNO__ )
+  gpsSerial.begin( UART5, 38400UL, UART5_TXD, UART5_RXD );
+#elif  defined(  __GPS_TYPE_MT3339__ )
+  gpsSerial.begin( UART5, 9600UL, UART5_TXD, UART5_RXD );
+#endif  /*__GPS_TYPE_FURUNO__ or __GPS_TYPE_MT3339__ */
+
+  /* generate a gnss instance. */
+  GNSS gnss( &gpsSerial );
+
+  /* configure ntp client. */
+  static const uint8_t ntp_server[] = { 133,243,238,164 };
+//  static const uint8_t ntp_server[] = { 192,168,100,69 };
+  NTP sntp( SNTP_CLIENT_SOCKET, ntp_server, NTP_PORT );
+
+  /* start ntp client at once. */
+  dly_tsk( 5 * 1000UL );
+  time_t second; uint16_t milli;
+  sntp.unix( &second, &milli );
+  unixTime = second + 1;
+  dly_tsk( 1000UL - milli );
+
+  /* start timer1 and timer8 for 1pps generator. */
+  time_t unixTimeBase = unixTime;
+  ppsGenerator();
+
+  while( 1 )
+  {
+    while( unixTime == unixTimeBase ) rot_rdq();
+    unixTimeBase = unixTime;
+
+    uint16_t year;
+    uint8_t month,day,hour,minute,second;
+    localDateTime( unixTimeBase,
+      &year, &month, &day, &hour, &minute, &second );
+    bd1702.setHour( hour );
+    bd1702.setMinute( minute );
+    bd1702.setSecond( second );
+
+    utcDateTime( unixTimeBase,
+      &year, &month, &day, &hour, &minute, &second );
+    GPS_GPZDA zda;
+    zda.hhmmss = (hour * 3600UL) + (minute * 60UL) + second + 0.0F;
+    zda.day = day;
+    zda.month = month;
+    zda.year = year;
+    zda.tzHour = 9;
+    zda.tzMinute = 0;
+    char gnssBuffer[100];
+    gnss.nmeaZDA( &zda, TYPE_GPS, gnssBuffer, sizeof(gnssBuffer) );
+    gnss.output( gnssBuffer );
+  }
+}
+
+
+/*******************************************************************************
+* Function Name  : RCC_Configuration
+* Description    : Configures the different system clocks.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void RCC_Configuration( void )
+{
+  /* Setup the microcontroller system. Initialize the Embedded Flash Interface,
+     initialize the PLL and update the SystemFrequency variable. */
+  SystemInit();
+
+  /* Enable GPIO clock */
+  GPIO gpio;
+  gpio.rccClockEnable( RCC_AFIO );
+  gpio.rccClockEnable( RCC_GPIOA );
+  gpio.rccClockEnable( RCC_GPIOB );
+  gpio.rccClockEnable( RCC_GPIOC );
+  gpio.rccClockEnable( RCC_GPIOD );
+
+  /* DMA1,2 clock enable */
+  RCC_AHBPeriphClockCmd( RCC_AHBPeriph_DMA1 | RCC_AHBPeriph_DMA2, ENABLE);
+
+#if 0
+  /* Enable spi1 clock enable */
+  RCC_APB2PeriphClockCmd( RCC_APB2Periph_SPI1, ENABLE );
+  RCC_APB2PeriphResetCmd( RCC_APB2Periph_SPI1, DISABLE );
+#endif
+}
+
+/*******************************************************************************
+* Function Name  : NVIC_Configuration
+* Description    : Configures Vector Table base location.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+//extern uint32_t _isr_vectorsflash_offs;
+void NVIC_Configuration( void )
+{
+  /* Set the Vector Table base location at 0x08000000+_isr_vectorsflash_offs */
+  NVIC_SetVectorTable( NVIC_VectTab_FLASH, 0 );  // VECT_TAB_OFFSET = 0
+}
+
+/* ----------------------------------------
+    timer interrupt handlers.
+---------------------------------------- */
+void msecInterrupt( void )
+{
+  tim1ms.setAutoReload( reload );
+  if( ++pps_millisecond_counter >= 1000 ) pps_millisecond_counter = 0;
+}
+
+void secInterrupt( void )
+{
+  unixTime++;
+}
+
